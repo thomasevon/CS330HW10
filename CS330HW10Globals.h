815c32422dfa7d9a6d126331ff8abfe144d01190
thomasevon <56008706+thomasevon@users.noreply.github.com>
2019-11-09 17:07:19 -0700

    Set up

---
+++ CS330HW10Globals.h
@@ -0,0 +1,78 @@
+// Thomas Evon
+// CS 330
+// CS330HW10Globals.h
+// header file that contains all globals used for CS330HW10.c
+
+#ifndef CS330HW10GLOBALS_H
+#define CS330HW10GLOBALS_H
+
+// globals:
+int CLOCKS;				// total process clocks
+int	L1IACCESSES;	 	// total L1I total accesses
+int	L1IHITS;			// total L1I hits
+int L1DACCESSES;		// total L1D total accesses
+int L1DHITS;			// total L1D hits
+int	L2ACCESSES;			// total L2 accesses
+int L2HITS;				// total L2 hits
+int BUFACCESSES;		// total buffer accesses
+int DISKACCESSES;		// total disk accesses
+int TLBACCESSES;		// total TLB accesses
+int TLBHITS;			// total TLB hits
+int	PAGEFAULTS;			// total page faults
+
+// structs:
+struct ca {
+	char typeOfAccess;
+	unsigned int va, pn, fn, offset, pa, L1Index, L2Index;
+	int bytes; 
+}; // currentAccess struct
+
+
+struct L1I {
+	int v, d;
+	unsigned int page;
+};
+
+
+struct L1D {
+	int v, d;
+	unsigned int page;
+};
+
+
+struct L2 {
+	int v, d;
+	unsigned int frame;
+};
+
+struct TLB {
+	int v;
+	unsigned int frame, page;
+};
+
+struct FT {
+	unsigned int frame;
+	int* freePtr; 	
+};
+
+struct BUF {
+	int v, d;
+	unsigned int frame; 
+};
+
+struct PT {
+	int v, d;
+	unsigned int frame; 
+};
+
+// instantiate structures:
+struct ca ca;			
+struct L1I L1I;
+struct L1D L1D;
+struct L2 L2; 
+struct TLB TLB;
+struct PT PT;
+struct BUF BUF; 
+struct FT FT;
+
+#endif 
